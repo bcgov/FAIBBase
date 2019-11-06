054624f5299a08edeb921eadc3d8a5312397c8a8
Luo <Yong.Luo@gov.bc.ca>
2019-11-05 16:05:41 -0800

    [checkMissing_remeas] the function was created to check missing observations for remeasured data

---
+++ R/checkMissing_remeas.R
@@ -0,0 +1,82 @@
+#' Check the missing measurements in repeatedly measured subject against intended measurements
+#'
+#' @description This function is to check missing measurements in repeatedly measured
+#'              subject against intended measurements. Note that this function allows the
+#'              regeneration, which means there may be missing measurements at the begining of
+#'              intended masurements. Additionally, the function may allow the missing measurements
+#'              after a subject died, depending on how \code{deadCode} is specified.
+#'
+#' @param subjectID character, Specifies subject ID, such as a tree id.
+#' @param measNo numeric, Measurement number with bigger value indicates a later measurement.
+#' @param intendedMeasNo numeric, The measurement number that a subject is intended measured.
+#' @param deadCode character, The code indicates the subject is dead. This arguement serves two purposes:
+#'                 1) switch the function whether check a subject under dead scenario (i.e., the missing
+#'                 measurement before last intended measurement). Setting \code{NULL} will turn off dead mode,
+#'                 arguement \code{LDStatus} will not be used in the function.
+#'                 2) if the dead mode is turn on (i.e., anything but \code{NULL}),
+#'                 this term determines which code will be used for dead. In this case, arguement \code{LSStatus}
+#'                 must be specified. By default, this term is set as \code{NULL}.
+#' @param LDStatus character, Live or dead status. This arguement is called only when \code{deadCode}
+#'                 is not set as \code{NULL}.
+#'
+#' @return A data table that contains pass information. TRUE indicates pass, while FALSE indicates
+#'         failure. The table also contains the missing measurements.
+#'
+#' @importFrom data.table ':=' data.table copy
+#' @author Yong Luo
+#' @export
+#' @rdname checkMissing_remeas
+checkMissing_remeas <- function(subjectID, measNo, intendedMeasNo,
+                                deadCode = NULL, LDStatus){
+  if(is.null(deadCode)){
+    thedata <- data.table(subjectID, measNo, LDStatus = 0)
+    if(nrow(thedata) != nrow(unique(thedata, by = c("subjectID", "measNo")))){
+      stop("Duplicate measurements were recorded for one subject, please check the duplicates.")
+    }
+  } else {
+    thedata <- data.table(subjectID, measNo, LDStatus)
+    if(nrow(thedata) != nrow(unique(thedata, by = c("subjectID", "measNo")))){
+      stop("Duplicate measurements were recorded for one subject, please check the duplicates.")
+    }
+    thedata[LDStatus == deadCode, newld := 1]
+    thedata[is.na(newld), newld := 0]
+    deadcheck <- checkLD_remeas(subjectID = thedata$subjectID,
+                                measNo = thedata$measNo,
+                                LDStatus = thedata$newld,
+                                liveCode = 0,
+                                deadCode = 1)
+    if(nrow(deadcheck[pass == FALSE,]) > 0){
+      stop("Please check the live and dead status before check missing observations.")
+    }
+    thedata_firstdead <- thedata[newld == 1,]
+    thedata_firstdead <- thedata_firstdead[, .(firstdead = min(measNo)),
+                                           by = "subjectID"]
+    thedata <- merge(thedata, thedata_firstdead,
+                     by = "subjectID", all.x = TRUE)
+    thedata <- thedata[is.na(firstdead) | measNo <= firstdead,
+                       .(subjectID, measNo, LDStatus = newld)]
+  }
+  fullmeas <- data.table(expand.grid(subjectID = unique(subjectID),
+                                     intendedMeasNo = intendedMeasNo))
+  twoends <- thedata[,.(firstMeasNo = min(measNo),
+                        lastMeasNo = max(measNo),
+                        lastDead = max(LDStatus)),
+                     by = "subjectID"]
+  twoends[lastDead != 1, lastMeasNo := max(intendedMeasNo)]
+
+  fullmeas <- merge(fullmeas, twoends,
+                    by = "subjectID", all.x = TRUE)
+  fullmeas <- fullmeas[firstMeasNo <= intendedMeasNo &
+                         lastMeasNo >= intendedMeasNo,]
+  fullmeas[, measNo := intendedMeasNo]
+  fullmeas <- merge(fullmeas, thedata[,.(subjectID, measNo, obs = 1, LDStatus)],
+                    by = c("subjectID", "measNo"), all.x = TRUE)
+  fullmeas[,':='(totaloobs = sum(obs, na.rm = TRUE),
+                 intendobs = length(obs)),
+           by = "subjectID"]
+  results <- rbind(unique(fullmeas[totaloobs == intendobs,.(subjectID, pass = TRUE, missingMeasNo = NA)],
+                          by = "subjectID"),
+                   fullmeas[is.na(obs),.(subjectID, pass = FALSE, missingMeasNo = measNo)])
+  results <- results[order(subjectID, missingMeasNo),]
+  return(results)
+}
---
+++ tests/testthat/test-checkMissing_remeas.R
@@ -0,0 +1,122 @@
+test_that("checkMissing_remeas.R: the missing measurements are not correctly checked.", {
+  library(data.table)
+  library(testthat)
+  ## check error
+  expect_error(checkMissing_remeas(subjectID = "1",
+                                   measNo = c(1, 2, 2, 3), # duplicate measurement 2
+                                   intendedMeasNo = 1:5,
+                                   deadCode = NULL)) # no dead mode
+  expect_error(checkMissing_remeas(subjectID = "1",
+                                   measNo = c(1, 2, 2, 3), # duplicate measurement 2
+                                   intendedMeasNo = 1:5,
+                                   deadCode = "D", # dead mode
+                                   LDStatus = c("0", "0", "1", "D")))
+
+  expect_error(checkMissing_remeas(subjectID = "1",
+                                   measNo = c(1:5),
+                                   intendedMeasNo = 1:10,
+                                   deadCode = "1",
+                                   LDStatus = c("0", "0", "1", "0", "1"))) # can not pass dead check
+  #             live, live, dead, live, dead
+  # check the pass scenario with dead mode off
+  thedata <- rbind(data.table(treeid = "1", measures = 1:5), # all 5 records
+                   data.table(treeid = "2", measures = 3:5)) # last three measurements
+
+  output <- checkMissing_remeas(subjectID = thedata$treeid,
+                                measNo =  thedata$measures,
+                                intendedMeasNo = 1:5,
+                                deadCode = NULL)
+  expect_equal(output,
+               data.table(subjectID = c("1", "2"),
+                          pass = TRUE,
+                          missingMeasNo = as.numeric(NA)))
+  rm(output)
+  # check the pass scenario with dead mode on
+  thedata <- rbind(data.table(treeid = "1",
+                              measures = 1:5,
+                              ld = 0), # all live records
+                   data.table(treeid = "2",
+                              measures = 1:3, # first 3 measurements
+                              ld = c(0, 0, 1)), # dead at 3
+                   data.table(treeid = "3",
+                              measures = 3:5, # last 3 measurements
+                              ld = c(0, 0, 1)), # dead at 5
+                   data.table(treeid = "4",
+                              measures = 3:5, # last 3 measurements
+                              ld = c(0, 1, 1)), # dead at 4 with 5 recorded as well
+                   data.table(treeid = "5",
+                              measures = 3:4, # middle 2 measurements
+                              ld = c(0, 1))) # dead at 4
+
+  output <- checkMissing_remeas(subjectID = thedata$treeid,
+                                measNo =  thedata$measures,
+                                intendedMeasNo = 1:5,
+                                deadCode = 1,
+                                LDStatus = thedata$ld)
+  expect_equal(output,
+               data.table(subjectID = c("1", "2", "3", "4", "5"),
+                          pass = TRUE,
+                          missingMeasNo = as.numeric(NA)))
+  rm(output)
+
+
+  ## check some failure scenarios with dead mode off,
+  ## check did not measure to the last intended measurement
+  output <- checkMissing_remeas(subjectID = thedata$treeid,
+                                measNo =  thedata$measures,
+                                intendedMeasNo = 1:5,
+                                deadCode = NULL)
+  expect_equal(output,
+               data.table(subjectID = c("1", "2", "2", "3", "4", "5"),
+                          pass = c(TRUE, FALSE, FALSE, TRUE, TRUE, FALSE),
+                          missingMeasNo = c(NA, 4, 5, NA, NA, 5)))
+  #    subjectID  pass missingMeasNo
+  # 1:         1  TRUE            NA
+  # 2:         2 FALSE             4
+  # 3:         2 FALSE             5
+  # 4:         3  TRUE            NA
+  # 5:         4  TRUE            NA
+  # 6:         5 FALSE             5
+  rm(output)
+
+
+
+  ## check failure cases with dead mode on
+  thedata1 <- thedata[!(treeid == "1" & measures == 3),] # remove measurement 3 of tree 1
+  thedata1 <- thedata1[!(treeid == "2" & measures == 2),] # remove measurement 2 of tree 2
+  thedata1 <- thedata1[!(treeid == "3" & measures == 4),] # remove measurement 4 of tree 3
+  output1 <- checkMissing_remeas(subjectID = thedata1$treeid,
+                                 measNo =  thedata1$measures,
+                                 intendedMeasNo = 1:5,
+                                 deadCode = 1,
+                                 LDStatus = thedata1$ld)
+  expect_equal(output1,
+               data.table(subjectID = c("1", "2", "3", "4", "5"),
+                          pass = c(FALSE, FALSE, FALSE, TRUE, TRUE),
+                          missingMeasNo = c(3, 2, 4, NA, NA)))
+  thedata1[, ld := as.character(ld)]
+  thedata1[treeid == 1 & measures == 2, ld := "test1"]
+  thedata1[treeid == 2 & measures == 1, ld := "test2"]
+  thedata1[treeid == 5 & measures == 3, ld := "test3"]
+  output2 <- checkMissing_remeas(subjectID = thedata1$treeid,
+                                 measNo =  thedata1$measures,
+                                 intendedMeasNo = 1:5,
+                                 deadCode = "1",
+                                 LDStatus = thedata1$ld)
+  expect_identical(output1, output2)
+  rm(output1, output2, thedata1)
+
+  ## check missing middle measurements with dead mode off
+  thedata <- thedata[treeid == "1" & measures != 4,] # missing 4th measurement
+  output <- checkMissing_remeas(subjectID = thedata$treeid,
+                                measNo =  thedata$measures,
+                                intendedMeasNo = 1:5,
+                                deadCode = NULL)
+  expect_equal(output,
+               data.table(subjectID = c("1"),
+                          pass = c(FALSE),
+                          missingMeasNo = 4))
+
+  rm(output)
+
+})
